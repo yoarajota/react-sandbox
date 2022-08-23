f70ce8a46060b9f12ebeb673021fad15de9d87f0
João Vítor Sberse <81939995+yoarajota@users.noreply.github.com>
2022-08-23 19:37:00 -0300

    😂

--- pages/reducer.js
+++ pages/reducer.js
@@ -1,3 +1,47 @@
-export default function Reducer() {
-  return <></>;
-}
+import { useReducer } from "react";
+
+const initialTodos = [
+  {
+    id: 1,
+    title: "Todo 1",
+    complete: false,
+  },
+  {
+    id: 2,
+    title: "Todo 2",
+    complete: false,
+  },
+];
+
+const initialState = 0;
+const reducer = (state, action) => {
+  switch (action) {
+    case "add":
+      return state + 10;
+    case "subtract":
+      return state - 10;
+    case "reset":
+      return 0;
+    default:
+      throw new Error("Unexpected action");
+  }
+};
+
+export default function reducer() {
+  const [state, func] = useReducer(reducer, initialState);
+
+  return (
+    <div>
+      <h2 style={{fontSize: state}}>{state}</h2>
+      <button onClick={() => func("add")}>
+        add
+      </button>
+      <button onClick={() => func("subtract")}>
+        subtract
+      </button>
+      <button onClick={() => func("reset")}>
+        reset
+      </button>
+    </div>
+  );
+};
